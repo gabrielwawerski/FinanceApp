0c096963145c13fd28dad753576a9fd180bec433
gabrielwawerski <gabriel.wawerski@gmail.com>
2025-11-20 20:36:17 +0100

    move service worker

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ import { initializeDatabase, seedPredefinedCategories } from "@db/db-util.js";
 // Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`${BASE}service-worker.js`, {type: 'module'})
+    navigator.serviceWorker.register(`./service-worker.js`, {type: 'module'})
        .then(reg => console.log('Service Worker registered:', reg.scope))
        .catch(err => console.error('Service Worker registration failed:', err));
   });
